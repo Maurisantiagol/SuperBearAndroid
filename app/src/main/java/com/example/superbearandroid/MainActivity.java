7089244a964204244762af7971815056af594c2a
maurisantiagol <msantiagod1901@alumno.ipn.mx>
2022-04-30 19:36:54 -0500

    Conexion y consulta en MainActivity haciendo consulta en la bd buscando todos los usuarioss e imprimiendolos en consola de ejecucion

--- app/src/main/java/com/example/superbearandroid/MainActivity.java
+++ app/src/main/java/com/example/superbearandroid/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.superbearandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Task().execute();
     }
 
     public void Signin(View view){
@@ -23,4 +31,31 @@ public class MainActivity extends AppCompatActivity {
         Intent Signup = new Intent(this, SignUp.class);
         startActivity(Signup);
     }
+    class Task extends AsyncTask<Void, Void, Void> {
+        String error="",records="";
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Class.forName("com.mysql.jdbc.Connection");
+                Connection connection= DriverManager.getConnection("jdbc:mysql://superbears.mysql.database.azure.com/superbear","alexisluisedgarmauriciomiguel","$Beartual1412AELMMFornite$1412mareenplatano777pillomaumiguelin");
+                Statement statement=connection.createStatement();
+                ResultSet resultSet=statement.executeQuery("Select * from musuario");
+
+                while(resultSet.next()){
+                    records+= resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3)+"\n";
+                }
+            } catch (Exception e) {
+                error =e.toString();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            System.out.println(records);
+            if (error != "")
+                System.out.println(error);
+            super.onPostExecute (aVoid);
+        }
+    }
+
 }
