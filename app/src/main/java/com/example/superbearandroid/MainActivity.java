e9ad025f6f4b5c4c1be4f7fe711d99159c4915e9
maurisantiagol <msantiagod1901@alumno.ipn.mx>
2022-05-25 18:55:14 -0500

    El controlador ya se puede utilizar en clases diferentes a MainActivity.java

--- app/src/main/java/com/example/superbearandroid/MainActivity.java
+++ app/src/main/java/com/example/superbearandroid/MainActivity.java
@@ -2,19 +2,26 @@ package com.example.superbearandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.superbearandroid.control.bd;
 
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+    private static  final String FILE_NAME = "NoAbrir.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +39,50 @@ public class MainActivity extends AppCompatActivity {
         Intent Signup = new Intent(this, SignUp.class);
         startActivity(Signup);
     }
+
     class Task extends AsyncTask<Void, Void, Void> {
         String error="",records="";
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-
                 Connection connection= bd.getConnection();
-                Statement statement=connection.createStatement();
-                ResultSet resultSet=statement.executeQuery("Select * from musuario");
+
+                String q = "SELECT id_usu FROM musuario WHERE cor_usu = ?";
+                PreparedStatement ps = connection.prepareStatement(q);
+                ps.setString(1, "maurisantiagol@gmail.com");
+                ResultSet resultSet = ps.executeQuery();
+                String ID = resultSet.toString();
+                System.out.println(ID);
+
 
                 while(resultSet.next()){
-                    records+= resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3)+"\n";
+                    records+= resultSet.getString(1) ;
+                }
+
+                String textoASalvar = ID;
+                FileOutputStream fos = null;
+
+                try {
+                    fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+                    fos.write(textoASalvar.getBytes(StandardCharsets.UTF_8));
+                    Log.d("tag1", "---------------------------------------------Fichero salvado en: " + getFilesDir() + "/" + FILE_NAME);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(fos != null){
+                        try {
+                            fos.close();
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
                 }
             } catch (Exception e) {
                 error =e.toString();
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Void aVoid) {
             System.out.println(records);
--- app/src/main/java/com/example/superbearandroid/control/bd.java
+++ app/src/main/java/com/example/superbearandroid/control/bd.java
@@ -1,28 +1,40 @@
 package com.example.superbearandroid.control;
+import android.os.StrictMode;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class bd {
 
 
-
-    public static Connection getConnection() throws ClassNotFoundException{
-        try{
-            Class.forName("com.mysql.jdbc.Connection");
-            Connection connection= DriverManager.getConnection("jdbc:mysql://superbear.mysql.database.azure.com:3306/superbear","alexisluisedgarmauriciomiguel","$Beartual1412AELMMFornite$1412mareenplatano777pillomaumiguelin");
-            Class.forName("com.mysql.jdbc.Driver");
+    public static Connection getConnection() throws ClassNotFoundException {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            String url, userName, password;
+            url = "jdbc:mysql://superbear.mysql.database.azure.com/superbear";
+            userName = "alexisluisedgarmauriciomiguel";
+            password = "$Beartual1412AELMMFornite$1412mareenplatano777pillomaumiguelin";
+            Connection conexion=DriverManager.getConnection(url, userName, password);
             System.out.println("Se conecto a la BD");
-            return connection;
+            return conexion;
 
 
-        }catch(SQLException sq){
+        } catch (SQLException sq) {
             System.out.println("Error al conectar la BD");
             System.out.println(sq.getMessage());
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error de clase");
             System.out.println(e.getMessage());
         }
         return null;
     }
+
+
+
+
+
+
 
 }
